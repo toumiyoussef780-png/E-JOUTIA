39d055a9fca75729d7981810aa81829596ce0fb4
toumiyoussef780-png <toumiyoussef780@gmail.com>
2026-03-06 00:25:32 +0100

    UPDATE FILES

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 window.FIREBASE_CONFIG = {
     apiKey: "AIzaSyANy2nOXrmpQ745j67dyrA4uHIKfOH62LA",
-    authDomain: "e-joutia.firebaseapp.com",
-    projectId: "e-joutia",
-    storageBucket: "e-joutia.appspot.com",
-    messagingSenderId: "206508757533",
-    appId: "1:206508757533:web:b1033c0aefd0d2c4aaacf4"
-  };
+  authDomain: "e-joutia.firebaseapp.com",
+  projectId: "e-joutia",
+  storageBucket: "e-joutia.appspot.com",
+  messagingSenderId: "206508757533",
+  appId: "1:206508757533:web:b1033c0aefd0d2c4aaacf4"
+};
