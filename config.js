62ad06dfdc43be69476d912fe6404d49b31f9197
toumiyoussef780-png <toumiyoussef780@gmail.com>
2026-03-05 23:33:25 +0100

    new file

---
+++ config.js
@@ -0,0 +1,8 @@
+window.FIREBASE_CONFIG = {
+    apiKey: "هنا apiKey",
+    authDomain: "هنا authDomain",
+    projectId: "هنا projectId",
+    storageBucket: "هنا storageBucket",
+    messagingSenderId: "هنا messagingSenderId",
+    appId: "هنا appId"
+  };
